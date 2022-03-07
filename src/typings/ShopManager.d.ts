8ec703190308bed451e64e909efe2235ee617a0e
bad-boy-discord <andreyzero123@gmail.com>
2022-03-07 20:25:39 +0300

    :pencil2: Fix ShopManager Types Typo.

--- src/typings/ShopManager.d.ts
+++ src/typings/ShopManager.d.ts
@@ -3,33 +3,27 @@ import { DBManager } from "./DBManager";
 import { BalanceManager } from "./BalanceManager";
 
 export declare interface ShopManager {
-    options: Options;
-    database: DBManager;
+  options: Options;
+  database: DBManager;
 
-    balance: BalanceManager;
+  balance: BalanceManager;
 }
 
 export declare class ShopManager {
-    constructor(options: Options);
+  constructor(options: Options);
 
-    create(
-        guildID: string,
-        item: EconomyGuildShopItem
-    ): Promise<EconomyGuildShopItem>;
+  create(
+    guildID: string,
+    item: EconomyGuildShopItem
+  ): Promise<EconomyGuildShopItem>;
 
-    delete(
-        guildID: string,
-        itemID: number
-    ): Promise<boolean>;
+  delete(guildID: string, itemID: number): Promise<boolean>;
+  all(guildID: string): Promise<boolean | EconomyGuildShopItem[]>;
 
-    update<K extends keyof EconomyGuildShopItem>(
-        guildID: string,
-        itemID: number,
-        key: K,
-        value: EconomyGuildShopItem[K]
-    ): Promise<boolean | EconomyGuildShopItem>;
-
-    all(
-        guildID: string
-    ): Promise<boolean | EconomyGuildShopItem>;
+  update<K extends keyof EconomyGuildShopItem>(
+    guildID: string,
+    itemID: number,
+    key: K,
+    value: EconomyGuildShopItem[K]
+  ): Promise<boolean | EconomyGuildShopItem>;
 }
